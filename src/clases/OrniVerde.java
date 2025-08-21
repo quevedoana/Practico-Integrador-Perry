85c75716de93ce9696d50a4b6e30ba53825eff3f
AntonioAssat <turcotomas280302@gmail.com>
2025-08-21 18:04:04 -0300

    Implemente de mamaPata

--- src/clases/OrniVerde.java
+++ src/clases/OrniVerde.java
@@ -1,7 +1,21 @@
 
 package clases;
 
+import java.util.Scanner;
 
-public class OrniVerde {
+
+public class OrniVerde implements MamaPata{
+
+    @Override
+    public void tocarOrgano() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do-Re-Mi");
+         System.out.println("Presiona Enter para continuar...");
+        sc.nextLine(); // Este método espera la entrada del usuario
+        System.out.println("Fa-Sol-La-Si");
+    }
+    public void tocarGuitorgan(){
+        
+    }
     
 }
