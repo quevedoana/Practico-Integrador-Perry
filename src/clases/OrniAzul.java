2233ce445c821f5da39ca6381e05cbefef1f2740
candeenaranjo <mariacandelacande04@gmail.com>
2025-08-22 12:43:09 -0300

    punto3y4

--- src/clases/OrniAzul.java
+++ src/clases/OrniAzul.java
@@ -1,30 +1,45 @@
-
 package clases;
+
+import java.util.Comparator;
 
 /**
  *
  * @author esteb
  */
-public class OrniAzul extends Castor{
-    private double propulsion;
+public class OrniAzul extends Castor {
 
-    public OrniAzul(double propulsion, double cola, double velocidad) {
+    private Double propulsion;
+
+    public OrniAzul(Double propulsion, double cola, double velocidad) {
         super(cola, velocidad);
         this.propulsion = propulsion;
     }
 
-    public double getPropulsion() {
+    public Double getPropulsion() {
         return propulsion;
     }
 
-    public void setPropulsion(double propulsion) {
+    public void setPropulsion(Double propulsion) {
         this.propulsion = propulsion;
     }
+
     @Override
-    public void nadar(){
-        double aux= velocidad + propulsion;
-        System.out.println("Esta nadando a una velocidad de "+aux+ " km/h");
+    public void nadar() {
+        double aux = velocidad + propulsion;
+        System.out.println("Esta nadando a una velocidad de " + aux + " km/h");
+
+    }
+
+    @Override
+    public String toString() {
         
+        return "Ornitorrinco Azul{" + " Propulsion: " + propulsion + " Cola: " + super.cola +" Velocidad: "+ super.velocidad +'}';
     }
     
+    public static Comparator<OrniAzul> ordenarPropu = new Comparator<OrniAzul>() {
+        @Override
+        public int compare(OrniAzul o1, OrniAzul o2) {
+            return o1.getPropulsion().compareTo(o2.getPropulsion());
+        }
+    };
 }
--- src/integrador_perry/Integrador_perry.java
+++ src/integrador_perry/Integrador_perry.java
@@ -6,6 +6,8 @@
 package integrador_perry;
 
 import clases.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
@@ -20,23 +22,15 @@ public class Integrador_perry {
     public static void main(String[] args) {
         //creando las instancias 
         Scanner sc = new Scanner(System.in);
+        ArrayList<OrniAzul> nadadores = new ArrayList();
 
-        //atributos orniverde
-        double aux1;//longitud de la cola
-        double aux2;//velocidad
-
-        //atributos blue
-        double aux3;//propulsion
-        double aux4;//longitud de la cola
-        double aux5;//velocidad
-
-        //atributos blui
-        double aux6;//propulsion
-        double aux7;//longitud de la cola
-        double aux8;//velocidad
+        int i;
+        double aux1;
+        double aux2;
+        double aux3;
 
         //bucles para instanciar a orniverde
-        System.out.println("creando a Esteban");
+        System.out.println("Creando a Orniverde");
 
         do {
             System.out.println("Ingrese la longitud de la cola");
@@ -53,95 +47,90 @@ public class Integrador_perry {
                 System.out.println("La velocidad no puede ser negativa ni superior a 8");
             }
         } while (aux2 < 0 || aux2 > 8);
-        OrniVerde esteban = new OrniVerde(aux1, aux2);
+        OrniVerde Cande = new OrniVerde(aux1, aux2);
 
         //bucles para crear a blue
-        System.out.println("creando a blue");
+        System.out.println("Creando a blue");
 
         do {
             System.out.println("Ingrese la propulsion(entre 5 y 10)");
-            aux3 = sc.nextDouble();
-            if (aux3 < 5 || aux3 > 10) {
+            aux1 = sc.nextDouble();
+            if (aux1 < 5 || aux1 > 10) {
                 System.out.println("La propulsion no puede ser menor a 5 ni mayor a 10");
             }
-        } while (aux3 < 5 || aux3 > 10);
+        } while (aux1 < 5 || aux1 > 10);
         do {
             System.out.println("Ingrese la longitud de la cola");
-            aux4 = sc.nextDouble();
-            if (aux4 <= 0) {
+            aux2 = sc.nextDouble();
+            if (aux2 <= 0) {
                 System.out.println("La longitud no puede ser negativa");
             }
-        } while (aux4 <= 0);
+        } while (aux2 <= 0);
         do {
 
             System.out.println("Ingrese la velocidad(entre 0 y 8)");
-            aux5 = sc.nextDouble();
-            if (aux2 < 0 || aux2 > 8) {
+            aux3 = sc.nextDouble();
+            if (aux3 < 0 || aux3 > 8) {
                 System.out.println("La velocidad no puede ser negativa ni superior a 8");
             }
-
-        } while (aux5 < 0 || aux5 > 8);
-        OrniAzul blue = new OrniAzul(aux3, aux4, aux5);
+        } while (aux3 < 0 || aux3 > 8);
+        OrniAzul blue = new OrniAzul(aux1, aux2, aux3);
 
         //bucles para crear a blui
-        System.out.println("creando a blui");
+        System.out.println("Creando a blui");
 
         do {
             System.out.println("Ingrese la propulsion(entre 5 y 10)");
-            aux6 = sc.nextDouble();
-            if (aux6 < 5 || aux6 > 10) {
+            aux1 = sc.nextDouble();
+            if (aux1 < 5 || aux1 > 10) {
                 System.out.println("La propulsion no puede ser menor a 5 ni mayor a 10");
             }
-        } while (aux6 < 5 || aux6 > 10);
+        } while (aux1 < 5 || aux1 > 10);
         do {
             System.out.println("Ingrese la longitud de la cola");
-            aux7 = sc.nextDouble();
-            if (aux7 <= 0) {
+            aux2 = sc.nextDouble();
+            if (aux2 <= 0) {
                 System.out.println("La longitud no puede ser negativa");
             }
-        } while (aux7 <= 0);
+        } while (aux2 <= 0);
         do {
 
             System.out.println("Ingrese la velocidad(entre 0 y 8)");
-            aux8 = sc.nextDouble();
-            if (aux8 < 0 || aux8 > 8) {
+            aux3 = sc.nextDouble();
+            if (aux3 < 0 || aux3 > 8) {
                 System.out.println("La velocidad no puede ser negativa ni superior a 8");
             }
-        } while (aux8 < 0 || aux8 > 8);
-        OrniAzul blui = new OrniAzul(aux6, aux7, aux8);
-
-        //ejecutando habilidades
-        
-        //orniverde
-        System.out.println("esteban muestra sus habilidades para nadar y tocar instrumentos");
-        esteban.nadar();
-        esteban.tocarGuitarra();
-        esteban.tocarOrgano();
-        esteban.tocarGuitorgan();
-        
-        //blue
-        System.out.println("blue muestra sus habilidades para tocar la guitarra y nadar con propulsion ");
-        blue.tocarGuitarra();
-        blue.nadar();
-
-        //blui
-        System.out.println("blui muestra sus habilidades para tocar la guitarra y nadar con propulsion ");
-        blui.tocarGuitarra();
-        blui.nadar();
+        } while (aux3 < 0 || aux3 > 8);
+        OrniAzul blui = new OrniAzul(aux1, aux2, aux3);
 
         //Arreglo de ornitorrincos
-        try {
-            Castor[] hermanosOrni = new Castor[3];
-            hermanosOrni[0] = esteban;
-            hermanosOrni[1] = blue;
-            hermanosOrni[2] = blui;
+        Castor[] arregloOrnis = new Castor[3];
+        try {           
+            arregloOrnis[0] = Cande;
+            arregloOrnis[1] = blue;
+            arregloOrnis[2] = blui;
         } catch (ArrayIndexOutOfBoundsException a) {
-            System.out.println("indice de arreglo no valido");
+            System.out.println("Indice de arreglo no valido");
         } catch (Exception e) {
-            System.out.println("error de " + e.getMessage());
+            System.out.println("Error de " + e.getMessage());
         } finally {
-            System.out.println("los ornitohermanos estan juntos al fin…");
+            System.out.println("Los ornitohermanos estan juntos al fin");
+        }
+
+        for (i = 0; i < arregloOrnis.length; i++) {
+            if(arregloOrnis[i] instanceof OrniAzul){
+                OrniAzul aux = (OrniAzul)arregloOrnis[i];
+                nadadores.add(aux);   
+                System.out.println(aux);
+            }          
+        }    
+        
+        nadadores.sort(OrniAzul.ordenarPropu);
+        
+        Iterator<OrniAzul> ite = nadadores.iterator(); 
+        while(ite.hasNext()){
+            System.out.println(nadadores);
+            break;
         }
     }
-
 }
